0326aaf3e7d9d018c2784d09df4d3854a250be74
nekit <nekit_p@ukr.net>
2018-07-23 19:18:25 +0300

    removing from list correctly

--- src/ua/avaj/simulator/Tower.java
+++ src/ua/avaj/simulator/Tower.java
@@ -4,20 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Tower {
-  private List<Flyable> observers = new LinkedList<>();
+    private List<Flyable> observers = new LinkedList<>();
+    private List<Flyable> observersToRemove = new LinkedList<>();
 
-  public void register(Flyable flyable){
-    observers.add(flyable);
-  }
-  public void unregister(Flyable flyable){
-    observers.remove(flyable);
-  }
-  protected void conditionsChanged(){
-    for (Flyable oneOfFlyableListeners : observers)
-    {
-      oneOfFlyableListeners.updateConditions();
+    public void register(Flyable flyable) {
+        observers.add(flyable);
     }
-  }
+
+    public void unregister(Flyable flyable) {
+        observersToRemove.add(flyable);
+    }
+
+    protected void conditionsChanged() {
+        for (Flyable oneOfFlyableListeners : observers) {
+            oneOfFlyableListeners.updateConditions();
+        }
+        for (Flyable oneOfGoneFlyableListeners : observersToRemove) {
+            observers.remove(oneOfGoneFlyableListeners);
+        }
+    }
 //  public List<Flyable> getObservers() {
 //    return observers;
 //  }
